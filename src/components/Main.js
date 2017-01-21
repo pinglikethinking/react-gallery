739dd29d307144c4c2a7894af4ad1ca604b00a36
pinglikethinking <pinglikethinking@gmail.com>
2017-01-21 14:34:20 +0800

    create stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,29 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片相关数据
+var imageDatas = require('../data/imageDatas.json');
+//将图片名信息转换为url
+imageDatas = (function(imageDatasArray){
+  for(var i = 0, j = imageDatasArray.length; i < j; i++){
+    var singleImageData = imageDatasArray[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDatasArray[i] = singleImageData;
+  }
+  return imageDatasArray;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
     );
   }
 }
